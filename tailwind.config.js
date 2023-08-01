375a66f0fc2cfa7fd4dd31b920d5e82720732dae
AdiPalguna <yandeadi86@gmail.com>
2023-08-01 14:39:37 +0800

    Add Search Box features

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main": "#4AADE8"
+      }
+    },
   },
   plugins: [],
 }
